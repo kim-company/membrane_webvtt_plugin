bcbffdf8bad1b5007b849d2f034418f928138fc4
Awlex <please no>
2024-09-23 11:36:20 +0200

    Initial commit

---
+++ lib/membrane_webvtt_plugin.ex
@@ -0,0 +1,18 @@
+defmodule MembraneWebvttPlugin do
+  @moduledoc """
+  Documentation for `MembraneWebvttPlugin`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MembraneWebvttPlugin.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/membrane_webvtt_plugin_test.exs
@@ -0,0 +1,8 @@
+defmodule MembraneWebvttPluginTest do
+  use ExUnit.Case
+  doctest MembraneWebvttPlugin
+
+  test "greets the world" do
+    assert MembraneWebvttPlugin.hello() == :world
+  end
+end

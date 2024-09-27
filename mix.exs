d965cdfce3d03e41a846b6fb92120c52c2aea386
Awlex <please no>
2024-09-27 14:57:48 +0200

    Reduece min elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MembraneWebvttPlugin.MixProject do
     [
       app: :membrane_webvtt_plugin,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
